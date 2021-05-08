65107fae47ea2f6e46f590aa15329f9ad1486954
Alexander Stoichkov <sashostoichkov@gmail.com>
2021-05-08 22:41:52 +0300

    add help

--- course_work/settings.py
+++ course_work/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'student',
     'study',
     'project',
+    'help',
     'exam',
     'note',
 ]
---
+++ help/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils import timezone
+
+from project.models import Project
+
+# Create your models here.
+class Help(models.Model):
+    date = models.DateField(default=timezone.now)
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'Консултация за ' + self.project
