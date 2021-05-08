1228b1cb3fa12f74afa1791bb1bb64062357845d
Alexander Stoichkov <sashostoichkov@gmail.com>
2021-05-08 18:42:12 +0300

    add student

---
+++ student/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    faculty_number = models.BigAutoField(primary_key=True, unique=True)
+
+    first_name = models.CharField(max_length=255)
+    father_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    year = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name + ' ( ' + self.year + ' )'
