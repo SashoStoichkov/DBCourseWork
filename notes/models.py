1fdbc968da493aa3b595644c36445d1a86831453
Alexander Stoichkov <sashostoichkov@gmail.com>
2021-05-08 21:55:13 +0300

    add notes

---
+++ notes/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from student.models import Student
+
+# Create your models here.
+class Note(models.Model):
+    note = models.TextField()
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'Бележка за ' + self.student.get_full_name()
--- student/models.py
+++ student/models.py
@@ -10,5 +10,8 @@ class Student(models.Model):
 
     year = models.CharField(max_length=50)
 
-    def __str__(self):
+    def get_full_name(self):
         return self.first_name + ' ' + self.last_name + ' ( ' + self.year + ' )'
+
+    def __str__(self):
+        return self.get_full_name()
